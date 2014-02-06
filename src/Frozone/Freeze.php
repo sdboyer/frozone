4cf6242dc727360bf1f6c471e0e09b7522bc9511
Sam Boyer <tech@samboyer.org>
2014-02-06 09:44:27 -0500

    Add two more AttemptWrite*() methods, for convenience.

--- src/Frozone/Freeze.php
+++ src/Frozone/Freeze.php
@@ -5,7 +5,8 @@ namespace Frozone;
 /**
  * Trait implementing Freezable.
  *
- * Include in your classes for sweet, chilly magic.
+ * Make sure to begin any state-mutating method in the composed class with
+ * a call to $this->attemptWrite(__METHOD__).
  */
 trait Freeze /* implements FreezableInterface */ {
 
@@ -18,33 +19,53 @@ trait Freeze /* implements FreezableInterface */ {
    */
   protected $_tfrozen = FALSE;
 
-  /**
-   * {@inheritdoc}
-   */
   public function freeze() {
+    $this->attemptWrite('', 'Object is already frozen.');
     $this->_tfrozen = TRUE;
 
     return $this;
   }
 
-  /**
-   * {@inheritdoc}
-   */
   public function isFrozen() {
     return $this->_tfrozen;
   }
 
   /**
-   * Checks if the asset collection is frozen, throws an exception if it is.
+   * Checks if the object is frozen and throws an exception if it is.
+   *
+   * @throws FrozenObjectException
+   */
+  protected function attemptWrite() {
+    if ($this->isFrozen()) {
+      throw new FrozenObjectException(sprintf('State-changing method called on instance of %s.', __CLASS__));
+    }
+  }
+
+  /**
+   * Checks if the object is frozen and throws an exception if it is.
    *
    * @param string $method
    *   The name of the method that was originally called.
    *
    * @throws FrozenObjectException
    */
-  protected function attemptWrite($method) {
+  protected function attemptWriteWithMethod($method) {
     if ($this->isFrozen()) {
       throw new FrozenObjectException(sprintf('State-changing method %s::%s called on a frozen object instance.', __CLASS__, $method));
+    }
+  }
+
+  /**
+   * Checks if the object is frozen and throws an exception if it is.
+   *
+   * @param string $msg
+   *   The message to use in the exception, if frozen.
+   *
+   * @throws FrozenObjectException
+   */
+  protected function attemptWriteWithMessage($msg) {
+    if ($this->isFrozen()) {
+      throw new FrozenObjectException($msg);
     }
   }
 }
